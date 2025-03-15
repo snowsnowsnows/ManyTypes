556503cee6f6412f9a6a2c20a68f30754811828d
snow <notpidgey@users.noreply.github.com>
2025-03-15 02:10:32 -0400

    setup project structure

--- manytypes-lib/src/main.cpp
+++ manytypes-lib/src/main.cpp
@@ -291,59 +291,7 @@ CXChildVisitResult visit_cursor( CXCursor cursor, CXCursor parent, CXClientData 
 
 void parse_header( const std::string& header_data )
 {
-    const std::filesystem::path current_path = std::filesystem::current_path( );
-    const std::filesystem::path stub_source = current_path / "stub_include.cpp";
 
-    std::ofstream include_stub( stub_source, std::ios::out | std::ios::trunc );
-    include_stub << header_data;
-
-    include_stub.close( );
-
-    std::vector<std::string> clang_args =
-    {
-        // "-I" + std::string(argv[1]),
-        "-x",
-        "c++",
-        "-fms-extensions",
-        "-Xclang",
-        "-ast-dump",
-        "-fsyntax-only",
-        "-fms-extensions",
-    };
-
-    std::vector<const char*> c_args;
-    for ( const auto& arg : clang_args )
-        c_args.push_back( arg.c_str( ) );
-
-    if ( const auto index = clang_createIndex( 0, 1 ) )
-    {
-        CXTranslationUnit tu = nullptr;
-        const auto error = clang_parseTranslationUnit2(
-            index,
-            stub_source.string( ).c_str( ),
-            c_args.data( ),
-            static_cast<int>(c_args.size( )),
-            nullptr,
-            0,
-            CXTranslationUnit_DetailedPreprocessingRecord |
-            CXTranslationUnit_PrecompiledPreamble |
-            CXTranslationUnit_SkipFunctionBodies |
-            CXTranslationUnit_ForSerialization,
-            &tu );
-
-        if ( error == CXError_Success )
-        {
-            const CXCursor cursor = clang_getTranslationUnitCursor( tu );
-            clang_visitChildren( cursor, visit_cursor, nullptr );
-        }
-        else
-        {
-            printf( "CXError: %d\n", error );
-            return EXIT_FAILURE;
-        }
-
-        clang_disposeTranslationUnit( tu );
-    }
 }
 
 int main( )

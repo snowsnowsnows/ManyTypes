76dbebe25b91f85590c68e854c7b9bae434b12b8
snow <notpidgey@users.noreply.github.com>
2025-08-03 19:50:09 -0400

    fixed sandbox output generation

--- manytypes-sandbox/main.cpp
+++ manytypes-sandbox/main.cpp
@@ -8,22 +8,17 @@ int main( int argc, char* argv[] )
     if ( argc < 4 )
     {
         std::cout << "Usage: " << argv[0] << " <header-file-path> <json-output-path> <isbit32>" << std::endl;
-        return 1;
+        return 2;
     }
 
     std::filesystem::path header_path = argv[1];
     std::filesystem::path out_json = argv[2];
-    bool isbit32 = std::string( argv[3] ) == "1";
 
-    std::filesystem::path source = header_path.parent_path() / "source.cpp";
-    {
-        std::ofstream f( source, std::ios::trunc );
-        f << "#include \"" << header_path.filename().string() << "\"";
-    }
+    bool isbit32 = std::string( argv[3] ) == "1";
 
     try
     {
-        auto db = mt::parse_root_source( source, isbit32 );
+        auto db = mt::parse_root_source( header_path, isbit32 );
 
         std::ofstream out_db( out_json );
         out_db << mt::create_x64dbg_database( *db );
@@ -32,7 +27,7 @@ int main( int argc, char* argv[] )
     }
     catch ( std::exception& exc )
     {
-        std::cout << exc.what() << std::endl;
+        std::cout << "error: " << exc.what() << std::endl;
         return 1;
     }
 

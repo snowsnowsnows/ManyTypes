8c938281ac76acbc56f884c4c3e20f7caafd4346
snow <notpidgey@users.noreply.github.com>
2025-08-03 19:43:55 -0400

    added 32 bit generation flag

--- manytypes-sandbox/main.cpp
+++ manytypes-sandbox/main.cpp
@@ -5,16 +5,17 @@
 
 int main( int argc, char* argv[] )
 {
-    if ( argc < 3 )
+    if ( argc < 4 )
     {
-        std::cout << "Usage: " << argv[0] << " <header-file-path> <json-output-path>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <header-file-path> <json-output-path> <isbit32>" << std::endl;
         return 1;
     }
 
     std::filesystem::path header_path = argv[1];
     std::filesystem::path out_json = argv[2];
-    std::filesystem::path source = header_path.parent_path() / "source.cpp";
+    bool isbit32 = std::string( argv[3] ) == "1";
 
+    std::filesystem::path source = header_path.parent_path() / "source.cpp";
     {
         std::ofstream f( source, std::ios::trunc );
         f << "#include \"" << header_path.filename().string() << "\"";
@@ -22,7 +23,7 @@ int main( int argc, char* argv[] )
 
     try
     {
-        auto db = mt::parse_root_source( source );
+        auto db = mt::parse_root_source( source, isbit32 );
 
         std::ofstream out_db( out_json );
         out_db << mt::create_x64dbg_database( *db );
